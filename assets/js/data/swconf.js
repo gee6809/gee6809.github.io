23449f27bc644a302e181aa20486716226dec475
Jeuk Kim <gee6809@gmail.com>
2024-06-07 20:36:06 +0900

    google verification

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717759666',resources: [
+    cacheName: 'chirpy-1717760143',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
