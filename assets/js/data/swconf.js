b57b61ed7265554886a0701b2eac8147f8b27654
Jeuk Kim <gee6809@gmail.com>
2024-06-09 13:31:13 +0900

    stf: post added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1717849894',resources: [
+    cacheName: 'chirpy-1717907458',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
