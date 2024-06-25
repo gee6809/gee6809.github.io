1c487fc567a7be2559f02bae8be42e3350054799
Jeuk Kim <gee6809@gmail.com>
2024-06-25 14:22:15 +0900

    debug_kernel: post added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719232582',resources: [
+    cacheName: 'chirpy-1719293347',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
