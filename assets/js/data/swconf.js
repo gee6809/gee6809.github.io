0b29086e42e64e9ffe3f67dffa62d02ab083961a
Jeuk Kim <gee6809@gmail.com>
2024-06-30 13:14:42 +0900

    kernel_debug: gdb-multiarch added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719715323',resources: [
+    cacheName: 'chirpy-1719720802',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
