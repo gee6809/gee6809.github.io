61515d15792eb1744382a8a7008fc2836ab947ed
Jeuk Kim <gee6809@gmail.com>
2024-07-01 21:36:31 +0900

    Test: remove tips box

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719836914',resources: [
+    cacheName: 'chirpy-1719837362',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
