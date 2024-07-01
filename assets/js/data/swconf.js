0fea96b406b657da63a1defc1f78594180130f87
Jeuk Kim <gee6809@gmail.com>
2024-07-01 21:29:13 +0900

    optimization: png to webp

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719720802',resources: [
+    cacheName: 'chirpy-1719836914',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
