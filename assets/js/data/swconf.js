725a94b256c881e8cb525b311649e1148de8265f
Jeuk Kim <gee6809@gmail.com>
2024-07-02 22:59:42 +0900

    optimization: add pages to robots.txt

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719928353',resources: [
+    cacheName: 'chirpy-1719928749',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
