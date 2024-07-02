384a7e36ddc46f1ea13c2a29fe281214f57f7b54
Jeuk Kim <gee6809@gmail.com>
2024-07-02 22:52:55 +0900

    galaxy-kvm_2: post added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1719837362',resources: [
+    cacheName: 'chirpy-1719928353',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
