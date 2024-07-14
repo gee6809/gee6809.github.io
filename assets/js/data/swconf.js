1135cadf2aefa8be00c1294b80894d753f7b6169
Jeuk Kim <gee6809@gmail.com>
2024-07-14 19:59:08 +0900

    stf: 내용 추가

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1720695207',resources: [
+    cacheName: 'chirpy-1720954735',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
