afc3d26d824b5b42994f068eb0a309da374638d5
Jeuk Kim <gee6809@gmail.com>
2024-07-31 22:08:01 +0900

    event_loop: post added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722137014',resources: [
+    cacheName: 'chirpy-1722431261',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
