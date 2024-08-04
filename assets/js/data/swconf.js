ef2b5be9fc3cc01a37c367f772c8bef3c6725e9a
Jeuk Kim <gee6809@gmail.com>
2024-08-04 19:46:41 +0900

    showModalBottomSheet: post added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722740670',resources: [
+    cacheName: 'chirpy-1722768395',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
