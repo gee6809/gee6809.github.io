a4cf907eb1dd1e29f6825ac81950bd7bffa0821c
Jeuk Kim <gee6809@gmail.com>
2024-10-22 23:14:28 +0900

    flutter_string: post added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1728307700',resources: [
+    cacheName: 'chirpy-1729606460',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
