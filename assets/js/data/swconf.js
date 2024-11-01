bb2e1e240037857935352fc8ec3c04293b62c2ce
Jeuk Kim <gee6809@gmail.com>
2024-11-01 13:42:48 +0900

    Admob: app-ads.txt 추가

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729606460',resources: [
+    cacheName: 'chirpy-1730436143',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
