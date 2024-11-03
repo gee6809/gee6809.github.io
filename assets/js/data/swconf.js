d179e429692e12a00c54ef0e050b1d7778825a8e
Jeuk Kim <gee6809@gmail.com>
2024-11-04 08:45:37 +0900

    mac_sunshine: post added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1730436143',resources: [
+    cacheName: 'chirpy-1730677524',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
