4a8045151f669354e020444cd38715cdc52740d1
Jeuk Kim <gee6809@gmail.com>
2024-12-24 23:26:23 +0900

    supabase_auth_google: post added

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733181343',resources: [
+    cacheName: 'chirpy-1735314213',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
